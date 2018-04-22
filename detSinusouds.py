81f301cc4a639a5d06e5e176e224c230800ecdef
Gxzzz <1153210418@stu.hit.edu.cn>
2018-04-22 08:22:06 +0800

    Get unvoiced intervals, interpreted by Python 2

---
+++ detSinusouds.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 14:32:14 2015
+Description:
+Inputs:
+Outputs:
+@author: Gurunath Reddy M
+
+"""
+
+import numpy as np
+from scipy.signal import get_window
+import dftStft as stft
+from scipy.fftpack import fft
+import peakDetectCorrect as pdc
+
+
+def getSinusoids(x, fs):
+    # FFT parameter setting
+    M = (20 * fs) / 1000.0  # 20ms window
+    N = 1024
+    H = M / 4
+    window = 'hamming'
+    w = get_window(window, M)
+
+    # Computing STFT magnitude and phase of the signal
+    mx, px = stft.stftAnal(x, fs, w, N, H)  # mx and px are the magnitude and phase spectrum
+    mxLinear = 10 ** (mx / 20.0)  # Linear spectrum may not be needed
+
+    # -------------------------------------------------------------------------------- #
+    # Hamming window spectrum
+    hN = N / 2
+    hM = M / 2
+    fftbuffer = np.zeros(N)
+    mX1 = np.zeros(N)
+    hamWin = np.hamming(M)
+    hamWin = hamWin / sum(hamWin)
+    fftbuffer[hN - hM:hN + hM] = hamWin
+
+    X = fft(fftbuffer)
+    mXDb = 20 * np.log10(abs(X))
+    mX1[:hN] = mXDb[hN:]
+    mX1[N - hN:] = mXDb[:hN]
+    mXLinear = 10 ** (mX1 / 20.0)
+    hamMainLobe = mXLinear[507:518]
+    # -------------------------------------------------------------------------------- #
+    # Process the spectrum upto only 5kHz
+    maxBinFreq = np.round((5000.0 * N) / fs)
+    widthHalfHamMainLobe = hamMainLobe.size / 2
+
+    sinThresh = 0.6
+    t = 0.001
+
+    detSinusoids = np.zeros([np.shape(mxLinear)[0], maxBinFreq])
+
+    # for i in range(np.shape(mxLinear)[0]):
+    for i in range(np.shape(mxLinear)[0]):
+        tempMag = mxLinear[i, :maxBinFreq]
+        ploc = pdc.peakDetection(tempMag, t)  # detect peak locations
+        collS = np.array([])
+        # Peak locations are available, now compute similarity between the measured and the ideal sinusoid(spectrum of hamming window)
+        index = np.where(ploc <= widthHalfHamMainLobe)[0]  # Remove the index which is less than length of half main lobe for correct indexing
+        ploc = np.delete(ploc, index)
+        index = np.where(ploc > (maxBinFreq - (widthHalfHamMainLobe + 1)))[0]  # Remove plocation which is close to maxfrequency bin considered
+        ploc = np.delete(ploc, index)
+
+        for j in range(np.size(ploc)):
+            begSilce = ploc[j] - widthHalfHamMainLobe
+            endSilce = ploc[j] + widthHalfHamMainLobe + 1
+            measuredSin = tempMag[begSilce:endSilce]
+            Am = np.sum(hamMainLobe * measuredSin)
+            mainLobeEng = np.sum(np.power(hamMainLobe, 2))
+            Am = Am / mainLobeEng
+            Em = np.sum(np.power((measuredSin - (Am * hamMainLobe)), 2))
+            S = 1 - (Em / np.sum(np.power(measuredSin, 2)))
+            collS = np.append(collS, S)
+        sinPeaksIndx = np.where(collS > sinThresh)[0]
+        sinPeaksBin = ploc[sinPeaksIndx]
+        sinPeaksMag = tempMag[sinPeaksBin]
+        for k in range(sinPeaksBin.size):
+            detSinusoids[i, sinPeaksBin[k] - 5:sinPeaksBin[k] + 5] = tempMag[sinPeaksBin[k] - 5:sinPeaksBin[k] + 5]
+
+    return detSinusoids, mxLinear, sinPeaksMag, sinPeaksBin
---
+++ getSSHVUV.py
@@ -0,0 +1,25 @@
+"""
+Created on Wed Mar 11 19:11:06 2015
+Find the voiced regions in the wave file and then modify pitch of each voiced region
+@author: gurunath
+"""
+
+import numpy as np
+
+
+def sshVUV(sshVal, H, fs, N):
+    # TODO: Threshold may not work all the time. Come up with some different solution
+    thresh = 0.01
+
+    sshVUV = sshVal > thresh  # Keep zeros at uv and ones at v regions
+
+    sshVUV = np.array(sshVUV, dtype=int)  # converting bool to logical to take diff
+
+    diffShhVal = np.diff(sshVUV)  # Take the difference of signal
+    diffShhVal = np.append(diffShhVal, diffShhVal[-1])  # Adjust the signal length
+
+    begVoic = np.where(diffShhVal == 1)[0]  # Values = 1 in diffSgfiltEpssp1 corresponds to the beg. instant of voiced segment
+
+    endVoic = np.where(diffShhVal == -1)[0]  # Values = -1 corresponds to UV regions
+
+    return begVoic, endVoic
